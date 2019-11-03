8f61cccdc1ac599f40a2123de153dba321bb563c
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-03 00:55:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,46 +17,48 @@ def flatten_a_o_a(aoa)
 end
 
 def movie_with_director_name(director_name, movie_data)
-  {
-    :title => movie_data[:title],
+  { :title => movie_data[:title],
     :worldwide_gross => movie_data[:worldwide_gross],
     :release_year => movie_data[:release_year],
     :studio => movie_data[:studio],
-    :director_name => director_name
-  }
+    :director_name => director_name }
 end
-
-
-# Your code after this point
 
 def movies_with_director_key(name, movies_collection)
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
   # returned by this method.
-  #
   # INPUT:
   # * name: A director's name
   # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
   # RETURN:
-  #
+  director_key = []
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+  interval = 0
+while interval < movies_collection.size do
+    movies_name_key = { :title => movies_collection[:title],
+      :worldwide_gross => movies_collection[:worldwide_gross],
+      :release_year => movies_collection[:release_year],
+      :studio => movies_collection[:studio],
+      :director_name => name }
+    director_key << movies_name_key
+    interval += 1
 end
-
+p director_key
+end
 
 def gross_per_studio(collection)
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
-  #
   # INPUT:
   # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
+  
   # RETURN:
-  #
+  gross = {}
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
 end
